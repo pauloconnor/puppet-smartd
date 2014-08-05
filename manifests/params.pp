649a39ec86275e7d075942083ae686847f54067e
Paul O'Connor <paul@poconnor.me>
2014-08-05 18:59:11 +0100

    Force service name

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,18 +14,13 @@
 #
 class smartd::params {
   $package_name       = 'smartmontools'
+  $service_name       = 'smartmontools'
   $service_ensure     = 'running'
   $devicescan         = true
   $devicescan_options = undef
   $devices            = []
   $mail_to            = 'root'
   $warning_schedule   = 'daily' # other choices: once, diminishing
-
-  if versioncmp($::smartmontools_version, 5.42) >= 0 {
-    $service_name       = 'smartmontools'
-  } else {
-    $service_name       = 'smartd'
-  }
 
   case $::osfamily {
     'FreeBSD': {
